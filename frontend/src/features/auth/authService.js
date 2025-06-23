f5defdd373eb63b1235ca428d1f1daa5bba9094b
JoonHugh <joonhugh2021@gmail.com>
2025-06-23 16:40:03 +0900

    trying to proxy through vercel

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -43,8 +43,8 @@ const update = async (userData) => {
     }
 
     console.log("💡 Sending update to:", API_URL + 'me');
-    console.log("🧾 Data:", userData);
-    console.log("🔐 Auth:", user?.token);
+console.log("🧾 Data:", userData);
+console.log("🔐 Auth:", user?.token);
     const response = await axios.put(API_URL + 'me', userData, config);
 
     // if (response.data) {
